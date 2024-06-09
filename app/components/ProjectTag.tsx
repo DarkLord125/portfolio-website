d5822da47c493f496732ed6ffda2d7eb5348759d
Ashwin Nair <hashwin381@gmail.com>
2024-06-09 19:02:28 +0530

    New: css updates

--- app/components/ProjectTag.tsx
+++ app/components/ProjectTag.tsx
@@ -12,7 +12,7 @@ export const ProjectTag = ({
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
   return (
     <button
-      className={`${buttonStyles} cursor-pointer rounded-full py-3 px-6 text-base sm:text-xl border-2`}
+      className={`${buttonStyles} cursor-pointer rounded-full py-3 px-6 text-sm sm:text-xl border-2`}
       onClick={() => onClick(name)}
     >
       {name}
