4ef60c0c72a3fa1db7f99615f0d23d3451688f13
Ashwin Nair <hashwin381@gmail.com>
2024-06-09 18:57:59 +0530

    new: mobile responsive changes

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -40,7 +40,7 @@ export const ProjectCard = ({
         </div>
       </div>
       <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
-        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <h5 className="text-base sm:text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
