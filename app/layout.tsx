b4965e056157e49e5775dd5224b0531de7748e6a
Ashwin Nair <hashwin381@gmail.com>
2024-07-13 19:24:48 +0530

    New: app logo updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.svg" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
