77354133e29f710c7ce4d520fe3a1085eb0bb40f
Ashwin Nair <hashwin381@gmail.com>
2024-07-13 19:31:02 +0530

    fix: app logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="24x24" type="image/png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
