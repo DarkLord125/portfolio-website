f9edfdf9bc58186a6e015d10e4389f0877c30e2f
Ashwin Nair <hashwin381@gmail.com>
2024-08-25 16:49:25 +0530

    New: experience content update

--- app/components/ExperienceSection.tsx
+++ app/components/ExperienceSection.tsx
@@ -5,24 +5,79 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Image from "next/image";
+import { ReactNode } from "react";
+
+interface ExperienceItemsProps {
+  id: number;
+  title: string;
+  location: string;
+  description: ReactNode;
+  date: string;
+  logo: string;
+}
+
+const textStyles = "text-black text-sm sm:text-base mt-1";
 
 export const ExperienceSection = () => {
-  const items = [
+  const items: ExperienceItemsProps[] = [
     {
       id: 1,
       title: "Software Engineer Intern",
       location: "MND Technologies Pvt Ltd",
-      description:
-        "Developed and implemented responsive UI components for the Vision Cure project. Wrote efficient back-end code using Nest.js and Prisma, and managed the PostgreSQL database. Employed Git, Github and Bitbucket for version control and collaborative code management.  ",
-      date: "June 2023 - present",
+      description: (
+        <ul className="list-disc px-4">
+          <li className={textStyles}>
+            Developed and implemented responsive UI components for a recruitment
+            platform using Next.js and Tailwind CSS.
+          </li>
+        </ul>
+      ),
+      date: "July 2024 - present",
       logo: "/images/experience/my_next_developer_logo.jpg",
     },
     {
       id: 2,
+      title: "Software Engineer",
+      location: "MND Technologies Pvt Ltd",
+      description: (
+        <ul className="list-disc px-4">
+          <li className={textStyles}>
+            Worked on the frontend and backend components for a resume builder
+            web app using Next.js, Express.js and MongoDB.
+          </li>
+          <li className={textStyles}>
+            Contributed to the frontend application for a pet services SaaS
+            start-up using Next.js, Tailwind CSS and AntDesign.
+          </li>
+          <li className={textStyles}>
+            Worked on the frontend and backend for an ophthalmic device company
+            to manage doctors and tests using Next.js, Nest.js and PostgreSQL.
+          </li>
+          <li className={textStyles}>
+            Employed Git, Github and Bitbucket for version control and
+            collaborative code management.
+          </li>
+        </ul>
+      ),
+      date: "June 2023 - June 2024",
+      logo: "/images/experience/my_next_developer_logo.jpg",
+    },
+    {
+      id: 3,
       title: "Research Intern",
       location: "KJ Somaiya College Of Engineering, Vidyavihar",
-      description:
-        "Used Track Anything library, to perform video segmentation on drone footage of forests. Published a dataset titled - Dronescape: A high-resolution drone footage dataset for tree region segmentation on IEEE DataPort.",
+      description: (
+        <ul className="list-disc px-4">
+          <li className={textStyles}>
+            Used Track Anything library, to perform video segmentation on drone
+            footage of forests.
+          </li>
+          <li className={textStyles}>
+            Published a dataset titled - Dronescape: A high-resolution drone
+            footage dataset for tree region segmentation on IEEE DataPort.
+          </li>
+        </ul>
+      ),
       date: "September 2022 - June 2023",
       logo: "/images/experience/kjsce-logo.png",
     },
@@ -34,9 +89,7 @@ export const ExperienceSection = () => {
         My Journey
       </h2>
       <div>
-        <VerticalTimeline
-          animate={false}
-        >
+        <VerticalTimeline animate={false}>
           {items.map((item) => (
             <VerticalTimelineElement
               key={item.id}
@@ -62,9 +115,9 @@ export const ExperienceSection = () => {
               <h4 className="vertical-timeline-element-subtitle text-black text-sm sm:text-base">
                 {item.location}
               </h4>
-              <p className="text-black text-sm sm:text-base">
-                {item.description}
-              </p>
+              {/* <p className="text-black text-sm sm:text-base"> */}
+              {item.description}
+              {/* </p> */}
             </VerticalTimelineElement>
           ))}
         </VerticalTimeline>
